c86cd81692eca224b69d6177b050cb79a68580a4
Adam Muinuddin <muinuddin.adam@gmail.com>
2023-01-14 19:08:39 -0500

    Commit 3 - Rules have been added

--- wordle.py
+++ wordle.py
@@ -3,15 +3,30 @@ import random
 ## List of words that cna be used for Wordle
 word_list = ['ABOUT', 'ABOVE', 'ABUSE', 'ACTOR', 'ACUTE', 'ADMIT', 'ADOPT', 'ADULT', 'AFTER', 'AGAIN', 'AGENT', 'AGREE', 'AHEAD', 'ALARM', 'ALBUM', 'ALERT', 'ALIKE', 'ALIVE', 'ALLOW', 'ALONE', 'ALONG', 'ALTER', 'AMONG', 'ANGER', 'ANGLE', 'ANGRY', 'APART', 'APPLE', 'APPLY', 'ARENA', 'ARGUE', 'ARISE', 'ARRAY', 'ASIDE', 'ASSET', 'AUDIO', 'AUDIT', 'AVOID', 'AWARD', 'AWARE', 'BADLY', 'BAKER', 'BASES', 'BASIC', 'BASIS', 'BEACH', 'BEGAN', 'BEGIN', 'BEGUN', 'BEING', 'BELOW', 'BENCH', 'BILLY', 'BIRTH', 'BLACK', 'BLAME', 'BLIND', 'BLOCK', 'BLOOD', 'BOARD', 'BOOST', 'BOOTH', 'BOUND', 'BRAIN', 'BRAND', 'BREAD', 'BREAK', 'BREED', 'BRIEF', 'BRING', 'BROAD', 'BROKE', 'BROWN', 'BUILD', 'BUILT', 'BUYER', 'CABLE', 'CALIF', 'CARRY', 'CATCH', 'CAUSE', 'CHAIN', 'CHAIR', 'CHART', 'CHASE', 'CHEAP', 'CHECK', 'CHEST', 'CHIEF', 'CHILD', 'CHINA', 'CHOSE', 'CIVIL', 'CLAIM', 'CLASS', 'CLEAN', 'CLEAR', 'CLICK', 'CLOCK', 'CLOSE', 'COACH', 'COAST', 'COULD', 'COUNT', 'COURT', 'COVER', 'CRAFT', 'CRASH', 'CREAM', 'CRIME', 'CROSS', 'CROWD', 'CROWN', 'CURVE', 'CYCLE', 'DAILY', 'DANCE', 'DATED', 'DEALT', 'DEATH', 'DEBUT', 'DELAY', 'DEPTH', 'DOING', 'DOUBT', 'DOZEN', 'DRAFT', 'DRAMA', 'DRAWN', 'DREAM', 'DRESS', 'DRILL', 'DRINK', 'DRIVE', 'DROVE', 'DYING', 'EAGER', 'EARLY', 'EARTH', 'EIGHT', 'ELITE', 'EMPTY', 'ENEMY', 'ENJOY', 'ENTER', 'ENTRY', 'EQUAL', 'ERROR', 'EVENT', 'EVERY', 'EXACT', 'EXIST', 'EXTRA', 'FAITH', 'FALSE', 'FAULT', 'FIBER', 'FIELD', 'FIFTH', 'FIFTY', 'FIGHT', 'FINAL', 'FIRST', 'FIXED', 'FLASH', 'FLEET', 'FLOOR', 'FLUID', 'FOCUS', 'FORCE', 'FORTH', 'FORTY', 'FORUM', 'FOUND', 'FRAME', 'FRANK', 'FRAUD', 'FRESH', 'FRONT', 'FRUIT', 'FULLY', 'FUNNY', 'GIANT', 'GIVEN', 'GLASS', 'GLOBE', 'GOING', 'GRACE', 'GRADE', 'GRAND', 'GRANT', 'GRASS', 'GREAT', 'GREEN', 'GROSS', 'GROUP', 'GROWN', 'GUARD', 'GUESS', 'GUEST', 'GUIDE', 'HAPPY', 'HARRY', 'HEART', 'HEAVY', 'HENCE', 'HENRY', 'HORSE', 'HOTEL', 'HOUSE', 'HUMAN', 'IDEAL', 'IMAGE', 'INDEX', 'INNER', 'INPUT', 'ISSUE', 'JAPAN', 'JIMMY', 'JOINT', 'JONES', 'JUDGE', 'KNOWN', 'LABEL', 'LARGE', 'LASER', 'LATER', 'LAUGH', 'LAYER', 'LEARN', 'LEASE', 'LEAST', 'LEAVE', 'LEGAL', 'LEVEL', 'LEWIS', 'LIGHT', 'LIMIT', 'LINKS', 'LIVES', 'LOCAL', 'LOGIC', 'LOOSE', 'LOWER', 'LUCKY', 'LUNCH', 'LYING', 'MAGIC', 'MAJOR', 'MAKER', 'MARCH', 'MARIA', 'MATCH', 'MAYBE', 'MAYOR', 'MEANT', 'MEDIA', 'METAL', 'MIGHT', 'MINOR', 'MINUS', 'MIXED', 'MODEL', 'MONEY', 'MONTH', 'MORAL', 'MOTOR', 'MOUNT', 'MOUSE', 'MOUTH', 'MOVIE', 'MUSIC', 'NEEDS', 'NEVER', 'NEWLY', 'NIGHT', 'NOISE', 'NORTH', 'NOTED', 'NOVEL', 'NURSE', 'OCCUR', 'OCEAN', 'OFFER', 'OFTEN', 'ORDER', 'OTHER', 'OUGHT', 'PAINT', 'PANEL', 'PAPER', 'PARTY', 'PEACE', 'PETER', 'PHASE', 'PHONE', 'PHOTO', 'PIECE', 'PILOT', 'PITCH', 'PLACE', 'PLAIN', 'PLANE', 'PLANT', 'PLATE', 'POINT', 'POUND', 'POWER', 'PRESS', 'PRICE', 'PRIDE', 'PRIME', 'PRINT', 'PRIOR', 'PRIZE', 'PROOF', 'PROUD', 'PROVE', 'QUEEN', 'QUICK', 'QUIET', 'QUITE', 'RADIO', 'RAISE', 'RANGE', 'RAPID', 'RATIO', 'REACH', 'READY', 'REFER', 'RIGHT', 'RIVAL', 'RIVER', 'ROBIN', 'ROGER', 'ROMAN', 'ROUGH', 'ROUND', 'ROUTE', 'ROYAL', 'RURAL', 'SCALE', 'SCENE', 'SCOPE', 'SCORE', 'SENSE', 'SERVE', 'SEVEN', 'SHALL', 'SHAPE', 'SHARE', 'SHARP', 'SHEET', 'SHELF', 'SHELL', 'SHIFT', 'SHIRT', 'SHOCK', 'SHOOT', 'SHORT', 'SHOWN', 'SIGHT', 'SINCE', 'SIXTH', 'SIXTY', 'SIZED', 'SKILL', 'SLEEP', 'SLIDE', 'SMALL', 'SMART', 'SMILE', 'SMITH', 'SMOKE', 'SOLID', 'SOLVE', 'SORRY', 'SOUND', 'SOUTH', 'SPACE', 'SPARE', 'SPEAK', 'SPEED', 'SPEND', 'SPENT', 'SPLIT', 'SPOKE', 'SPORT', 'STAFF', 'STAGE', 'STAKE', 'STAND', 'START', 'STATE', 'STEAM', 'STEEL', 'STICK', 'STILL', 'STOCK', 'STONE', 'STOOD', 'STORE', 'STORM', 'STORY', 'STRIP', 'STUCK', 'STUDY', 'STUFF', 'STYLE', 'SUGAR', 'SUITE', 'SUPER', 'SWEET', 'TABLE', 'TAKEN', 'TASTE', 'TAXES', 'TEACH', 'TEETH', 'TERRY', 'TEXAS', 'THANK', 'THEFT', 'THEIR', 'THEME', 'THERE', 'THESE', 'THICK', 'THING', 'THINK', 'THIRD', 'THOSE', 'THREE', 'THREW', 'THROW', 'TIGHT', 'TIMES', 'TIRED', 'TITLE', 'TODAY', 'TOPIC', 'TOTAL', 'TOUCH', 'TOUGH', 'TOWER', 'TRACK', 'TRADE', 'TRAIN', 'TREAT', 'TREND', 'TRIAL', 'TRIED', 'TRIES', 'TRUCK', 'TRULY', 'TRUST', 'TRUTH', 'TWICE', 'UNDER', 'UNDUE', 'UNION', 'UNITY', 'UNTIL', 'UPPER', 'UPSET', 'URBAN', 'USAGE', 'USUAL', 'VALID', 'VALUE', 'VIDEO', 'VIRUS', 'VISIT', 'VITAL', 'VOICE', 'WASTE', 'WATCH', 'WATER', 'WHEEL', 'WHERE', 'WHICH', 'WHILE', 'WHITE', 'WHOLE', 'WHOSE', 'WOMAN', 'WOMEN', 'WORLD', 'WORRY', 'WORSE', 'WORST', 'WORTH', 'WOULD', 'WOUND', 'WRITE', 'WRONG', 'WROTE', 'YIELD', 'YOUNG', 'YOUTH']
 
+
 ## Selecting a random word
 wordleword = random.choice(word_list)
-print(wordleword)
+#print(wordleword)
 
-## Coding the scoring algorithm
-#userguess = input("Input a 5 letter word: ")
-#wordleguess = userguess.upper()
+#####
+## Explaining how to code the game
+    # Give the rules to the user
+    # Take the user guess
+    # Check if the first letter is right
+    # Check if the first letter is in the right spot
+    # If the both are true then let the user know
+    # If it exists then let the user know
+    # If neither are true state that first letter is not a letter in the word
+    # Check if the user guesses the correct word 
+    # If correct congratulate them, if not tell them to try again
+    # Repeat the process for a total of 6 guesses
+    # Let the user know the game is over whether they guess the word or take all 6 guesses
 
-# User gets 6 guesses
+# Explaining the rules to the user
+print("Welcome to Wordle, the rules to the game are simple: \nYou get 6 guesses to guess the word that The Wordler is thinking of. \nThe Wordler will tell you give you information about each of your guesses. \nHe will tell you which letters in your guess exist \nHe will tell you if those letters are in the correct place . \nHe will tell you which letters in your guess do not exist. \nYou must use your logical deduction skills to beat The Wordler, good luck!\n\n\n")
+
+
+## User gets 6 guesses
 for z in range(6):
     # User guess automatically wrong
     firstletterexist = False
@@ -25,12 +40,12 @@ for z in range(6):
     fourthletterspot = False
     fifthletterspot = False
 
-    # Taking the users word
+    ## Taking the users word
     print("Guess number", z+1)
     userguess = input("Input a 5 letter word: ")
     wordleguess = userguess.upper()
 
-    # Checking if the letters are correct
+    ## Checking if the letters are correct
     for a in range(5):
         for b in range(5):
             if wordleguess[a] == wordleword[b] and a == 0:
@@ -44,7 +59,7 @@ for z in range(6):
             elif wordleguess[a] == wordleword[b] and a == 4:
                 fifthletterexist = True
 
-    # Checking if the letters are in the right spot
+    ## Checking if the letters are in the right spot
     for c in range(5):
         for d in range(5):
             if wordleguess[c] == wordleword[d] and c == d and 0 == c:
@@ -58,7 +73,7 @@ for z in range(6):
             if wordleguess[c] == wordleword[d] and c == d and 4 == c:
                 fifthletterspot = True
 
-    # Telling user the result of their guess
+    ## Telling user the result of their guess
     correct = "exists and is in the correct position"
     partially = "exists but is not in the correct position"
     incorrect = "does not exist"
@@ -97,14 +112,15 @@ for z in range(6):
     else:
         print(wordleguess[4], incorrect)
 
-    # Checking if their guess is correct
+    ## Checking if their guess is correct
     if firstletterexist and firstletterspot and secondletterexist and secondletterspot and thirdletterexist and thirdletterspot and fourthletterexist and fourthletterspot and fifthletterexist and fifthletterspot == True:
         print("Congratulations you win the game")
         break
     else:
-        print("Try again")
+        print("Try again\n")
 
-"""
+print("The word was", wordleword)
+""" This was all code for determining if the program was returning the correct true and false values in terms of the existentialness of letters and their placement
     for e in range(1):
         if firstletterexist == True:
             print(firstletterexist)
@@ -148,10 +164,3 @@ for z in range(6):
         else:
             print(False)
 """
-
-#Take the user guess
-#See if the first letter is right
-#See if the first letter is in the right spot
-#If the both are true then let the user know
-#If it exists then let the user know
-#If neither are true state that first letter is not a letter in the word
